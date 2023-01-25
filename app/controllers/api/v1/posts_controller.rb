1fcacb7e69a41c80bb365cdbbcc5c9a5b3e5516b
juanequex <ramon.g.uz37@gmail.com>
2023-01-24 23:53:15 -0600

    Add method to enable the crud

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,13 +1,46 @@
 class Api::V1::PostsController < ApplicationController
+  # GET data for /posts
   def index
+    @posts = Posts.all
+    render json: @posts
   end
 
+  # Get data for only one posts
+  def show
+    render json: @post
+  end
+
+  # POST a new post /posts
   def create
+    @post = Post.new(post_params)
+    if @poss.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
+  # PUT /posts/1
   def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post.destroy
+    head :no_content
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
